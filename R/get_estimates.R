3230dc9524c59acb6b869a9ea6f46c240e5458b0
Eric Oh <ericoh17@gmail.com>
2019-06-29 15:31:48 -0400

    have function return a list containing data frames of estimates and SEs for each method

--- R/get_estimates.R
+++ R/get_estimates.R
@@ -1,4 +1,37 @@
-
+#' Runs RRCME procedure
+#'
+#' Runs some combination of Regression
+#' Calibration, Risk set Regression
+#' Calibration, Generalized Raking
+#' Regression Calibration, and 
+#' Generalized Raking Naive as specified 
+#' by the estimators input. 
+#' Log hazard ratios and corresponding
+#' standard errors are returned.
+#'
+#' @param full_dat Data for full cohort 
+#' containing error-prone variables
+#' 
+#' @param valid_dat Validation subset
+#' from full cohort containing
+#' error-free variables
+#' 
+#' @param num_boot Number of bootstrap 
+#' replicates
+#' 
+#' @param sampling_type Sampling design
+#' for validation subset
+#' 
+#' @param estimators Vector of strings 
+#' indicating which estimators to run
+#' 
+#' @return Matrix of estimates and 
+#' standard errors
+#' 
+#' @importFrom boot boot
+#' 
+#' @rdname get_estimates
+#' @export
 CalcCorrectedlogHR <- function(full_dat, valid_dat, 
                                num_boot = 250, sampling_type,
                                estimators = c("RC", "RSRC", "GRRC", "GRN")) {
@@ -21,10 +54,10 @@ CalcCorrectedlogHR <- function(full_dat, valid_dat,
     stop("Validation data must be a subset of the full data")
   }
   
-  if (num_boot < 100) {
-    stop("Number of bootstrap replicates should be at least
-         100 to ensure proper confidence intervals")
-  }
+  #if (num_boot < 100) {
+  #  stop("Number of bootstrap replicates should be at least
+  #       100 to ensure proper confidence intervals")
+  #}
   
   full_dat$time[full_dat$randomized == TRUE] <- valid_dat$time
   full_dat$delta[full_dat$randomized == TRUE] <- valid_dat$delta
@@ -39,10 +72,12 @@ CalcCorrectedlogHR <- function(full_dat, valid_dat,
     
   }
   
+  results_lst <- list()
+  
   if ("RC" %in% estimators) {
     
     RC_fit <- FitRCModel(valid_dat, full_dat, 
-                         sampling_scheme, 
+                         sampling_type, 
                          return_coef = TRUE)
     
     if (sampling_type == "cc") {
@@ -52,7 +87,7 @@ CalcCorrectedlogHR <- function(full_dat, valid_dat,
                       RunRCBootstrap, 
                       strata = factor(full_dat$cc_strata), 
                       R = num_boot,
-                      sampling_type = sampling_scheme)
+                      sampling_type = sampling_type)
       
     } else if (sampling_type == "srs") {
       
@@ -61,14 +96,25 @@ CalcCorrectedlogHR <- function(full_dat, valid_dat,
                       RunRCBootstrap, 
                       strata = factor(full_dat$randomized), 
                       R = num_boot,
-                      sampling_type = sampling_scheme)
+                      sampling_type = sampling_type)
     }
+    
+    RC_mat <- matrix(c(RC_fit[[1]],
+                       sd(RC_boot$t[,1], na.rm = TRUE),
+                       RC_fit[[2]],
+                       sd(RC_boot$t[,2], na.rm = TRUE)),
+                     nrow = 2, byrow = TRUE)
+    
+    rownames(RC_mat) <- c("beta_x", "beta_z")
+    colnames(RC_mat) <- c("Estimate", "Standard Error")
+    
+    results_lst[["RC"]] <- as.data.frame(RC_mat)
   }
 
   if ("RSRC" %in% estimators) {
     
     RSRC_fit <- FitRSRCModel(valid_dat, full_dat, 
-                             sampling_scheme,
+                             sampling_type,
                              RC_fit[[1]], 
                              RC_fit[[2]])
     
@@ -79,7 +125,7 @@ CalcCorrectedlogHR <- function(full_dat, valid_dat,
                         RunRSRCBootstrap,
                         strata = factor(full_dat$cc_strata), 
                         R = num_boot,
-                        sampling_type = sampling_scheme,
+                        sampling_type = sampling_type,
                         beta_x_start = RC_fit[[1]], 
                         beta_z_start = RC_fit[[2]])
       
@@ -90,18 +136,29 @@ CalcCorrectedlogHR <- function(full_dat, valid_dat,
                         RunRSRCBootstrap,
                         strata = factor(full_dat$randomized), 
                         R = num_boot,
-                        sampling_type = sampling_scheme,
+                        sampling_type = sampling_type,
                         beta_x_start = RC_fit[[1]], 
                         beta_z_start = RC_fit[[2]])
       
     }
+    
+    RSRC_mat <- matrix(c(RSRC_fit[[1]],
+                         sd(RSRC_boot$t[,1], na.rm = TRUE),
+                         RSRC_fit[[2]],
+                         sd(RSRC_boot$t[,2], na.rm = TRUE)),
+                       nrow = 2, byrow = TRUE)
+    
+    rownames(RSRC_mat) <- c("beta_x", "beta_z")
+    colnames(RSRC_mat) <- c("Estimate", "Standard Error")
+    
+    results_lst[["RSRC"]] <- as.data.frame(RSRC_mat)
   }
 
   if ("GRRC" %in% estimators) {
     
     GRRC_fit <- FitRakingModel(valid_dat, full_dat, 
                                mod_rake = "RC", 
-                               sampling_scheme)
+                               sampling_type)
     
     if (sampling_type == "cc") {
       
@@ -111,7 +168,7 @@ CalcCorrectedlogHR <- function(full_dat, valid_dat,
                         strata = factor(full_dat$cc_strata), 
                         R = num_boot,
                         mod_rake = "RC", 
-                        sampling_type = sampling_scheme)
+                        sampling_type = sampling_type)
       
     } else if (sampling_type == "srs") {
       
@@ -121,16 +178,27 @@ CalcCorrectedlogHR <- function(full_dat, valid_dat,
                         strata = factor(full_dat$randomized), 
                         R = num_boot,
                         mod_rake = "RC", 
-                        sampling_type = sampling_scheme)
+                        sampling_type = sampling_type)
       
     }
+    
+    GRRC_mat <- matrix(c(GRRC_fit[[1]],
+                         sd(GRRC_boot$t[,1], na.rm = TRUE),
+                         GRRC_fit[[2]],
+                         sd(GRRC_boot$t[,2], na.rm = TRUE)),
+                       nrow = 2, byrow = TRUE)
+    
+    rownames(GRRC_mat) <- c("beta_x", "beta_z")
+    colnames(GRRC_mat) <- c("Estimate", "Standard Error")
+    
+    results_lst[["GRRC"]] <- as.data.frame(GRRC_mat)
   }
 
   if ("GRN" %in% estimators) {
     
     GRN_fit <- FitRakingModel(valid_dat, full_dat, 
                               mod_rake = "naive", 
-                              sampling_scheme)
+                              sampling_type)
     
     if (sampling_type == "cc") {
       
@@ -140,7 +208,7 @@ CalcCorrectedlogHR <- function(full_dat, valid_dat,
                        strata = factor(full_dat$cc_strata), 
                        R = num_boot,
                        mod_rake = "naive", 
-                       sampling_type = sampling_scheme)
+                       sampling_type = sampling_type)
       
     } else if (sampling_type == "srs") {
       
@@ -150,10 +218,23 @@ CalcCorrectedlogHR <- function(full_dat, valid_dat,
                        strata = factor(full_dat$randomized), 
                        R = num_boot,
                        mod_rake = "naive", 
-                       sampling_type = sampling_scheme)
+                       sampling_type = sampling_type)
       
     }
+    
+    GRN_mat <- matrix(c(GRN_fit[[1]],
+                        sd(GRN_boot$t[,1], na.rm = TRUE),
+                        GRN_fit[[2]],
+                        sd(GRN_boot$t[,2], na.rm = TRUE)),
+                      nrow = 2, byrow = TRUE)
+    
+    rownames(GRN_mat) <- c("beta_x", "beta_z")
+    colnames(GRN_mat) <- c("Estimate", "Standard Error")
+    
+    results_lst[["GRN"]] <- as.data.frame(GRN_mat)
   }
+  
+  return(results_lst)
 
 }
 
